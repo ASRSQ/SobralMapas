3811e54e664bd0f8f36b97a1c7e7b6d31771b7a3
ASRSQ <a.s.rwar@gmail.com>
2024-08-19 21:47:20 -0300

    Resolvendo conflitos durante o merge da branch front-end-updates

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -11,11 +11,8 @@
     <meta http-equiv="Content-Security-Policy">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
-<<<<<<< HEAD
 
 
-=======
->>>>>>> front-end-updates
 </head>
 
 <body>
